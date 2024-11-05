caae2646a74fe5fefd2214c12bf237b3db63fd48
Neordan <melhian.adam@gmail.com>
2024-11-05 15:09:25 +0100

    Ajout des fonctions pour voir, modifier ou supprimer un produit

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -18,4 +18,21 @@ class ProductController extends Controller
         $product = Product::create($request->all());
         return response()->json($product, 201);
     }
+    public function show($id)
+    {
+        $product = Product::findOrFail($id);
+        return response()->json($product);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $product = Product::findOrFail($id);
+        $product->update($request->all());
+        return response()->json($product);
+    }
+    public function destroy($id)
+    {
+        Product::destroy($id);
+        return response()->json(null, 204);
+    }
 }
