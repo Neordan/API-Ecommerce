97035651737d24ac73dac699ace76ee8bcea22b6
Neordan <melhian.adam@gmail.com>
2024-11-05 14:32:43 +0100

    Ajout de la route pour modifier un produit

--- routes/route.php
+++ routes/route.php
@@ -6,3 +6,4 @@ use Illuminate\Routing\Route;
 Route::get('/products', [ProductController::class, 'index']);        
 Route::post('/products', [ProductController::class, 'store']);       
 Route::get('/products/{id}', [ProductController::class, 'show']);    
+Route::put('/products/{id}', [ProductController::class, 'update']);   
