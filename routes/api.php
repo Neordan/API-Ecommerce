b9f03d62ebed7ce390d95ac0e96aa60e63d3a83f
Neordan <melhian.adam@gmail.com>
2024-11-05 17:16:35 +0100

    Renommage du fichier des routes de l'api

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,12 @@
 <?php
 
 use App\Http\Controllers\Api\ProductController;
-use Illuminate\Routing\Route;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/products', [ProductController::class, 'index']);        
 Route::post('/products', [ProductController::class, 'store']);       
 Route::get('/products/{id}', [ProductController::class, 'show']);    
 Route::put('/products/{id}', [ProductController::class, 'update']);   
 Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+
